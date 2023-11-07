b282217d3c05a8c29c6b61fa951a3eeb33f60829
Amirreza Noori <amirreza.noori@yahoo.com>
2023-11-07 19:28:07 +0330

    initial fetch melivecode users API

---
+++ app.js
@@ -0,0 +1,20 @@
+let section = document.querySelector("section");
+
+fetch("https://www.melivecode.com/api/users")
+  .then((res) => res.json())
+  .catch((err) => alert("error"))
+  .then((res) => {
+    section.innerHTML = res
+      .map(
+        ({ fname, lname, avatar }) => `
+            <div class="card">
+            <img src="${avatar}" width="100%">
+            <h2>First Name:</h2>
+            <span class="firstName">${fname}</span>
+            <h2>last Name:</h2>
+            <span class="lastName">${lname}</span>
+            </div>
+            `
+      )
+      .join("");
+  });
